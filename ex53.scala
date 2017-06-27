ccf9a5d648a5700702f8310fb3ac8805c9ce75f7
S Fujimoto <unicoshun@gmail.com>
2017-06-27 16:59:38 +0900

    ~53 ex6

--- ex53.scala
+++ ex53.scala
@@ -1,33 +1,47 @@
 /*  / sfujimoto: 2017/06/27 6:58 */
 import com.atomicscala.AtomicTest._
 
-case class Mobility(val mobility:String)
+trait Ability
 
-case class Vision(val vision:String)
+case class Mobility(val mobility:String) extends Ability
+case class Vision(val vision:String) extends Ability
+case class Manipulator(val manipulator:String) extends Ability
 
-
-case class Manipulator(val manipulator:String)
-
-class Robot(m:Mobility, v:Vision, mani:Manipulator) {
-  override def toString: String = s"$m, $v, $mani"
+class Robot {
+  var mobility = Vector[Mobility]()
+  var vision = Vector[Vision]()
+  var manipulator = Vector[Manipulator]()
+  def +(m:Mobility) = {
+    mobility = mobility :+ m
+    this
+  }
+  def +(v:Vision) = {
+    vision = vision :+ v
+    this
+  }
+  def +(m:Manipulator) = {
+    manipulator = manipulator :+ m
+    this
+  }
+  override def toString = mobility.mkString(", ") + " | " + vision.mkString(", ") + " | " + manipulator.mkString(", ")
 }
-val walker = new Robot(
-  Mobility("Legs"),
-  Vision("Visible Spectrum"),
-  Manipulator("Magnet"))
-walker is "Mobility(Legs), " +
-  "Vision(Visible Spectrum)," +
-  " Manipulator(Magnet)"
-val crawler = new Robot(
-  Mobility("Treads"),
-  Vision("Infrared"),
-  Manipulator("Claw"))
-crawler is "Mobility(Treads)," +
-  " Vision(Infrared), " +
-  "Manipulator(Claw)"
-val arial = new Robot(
-  Mobility("Propeller"),
-  Vision("UV"),
-  Manipulator("None"))
-arial is "Mobility(Propeller)," +
-  " Vision(UV), Manipulator(None)"
+
+///// 3
+
+
+//// 6
+
+
+val bot = new Robot +
+  Mobility("Propeller") +
+  Mobility("Legs") +
+  Vision("UV") +
+  Vision("Visible Spectrum") +
+  Manipulator("Magnet") +
+  Manipulator("Claw")
+
+bot is "Mobility(Propeller)," +
+  " Mobility(Legs) | Vision(UV)," +
+  " Vision(Visible Spectrum) |" +
+  " Manipulator(Magnet)," +
+  " Manipulator(Claw)"
